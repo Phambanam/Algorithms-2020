86f975fe649d063908875a8f0261c77d4c30352f
Phambanam <54939419+Phambanam@users.noreply.github.com>
2020-09-19 02:48:57 +0300

    Pham Ba Nam

--- src/lesson2/JavaAlgorithms.java
+++ src/lesson2/JavaAlgorithms.java
@@ -3,15 +3,20 @@ package lesson2;
 import kotlin.NotImplementedError;
 import kotlin.Pair;
 
-@SuppressWarnings("unused")
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaAlgorithms {
     /**
      * Получение наибольшей прибыли (она же -- поиск максимального подмассива)
      * Простая
-     *
+     * <p>
      * Во входном файле с именем inputName перечислены цены на акции компании в различные (возрастающие) моменты времени
      * (каждая цена идёт с новой строки). Цена -- это целое положительное число. Пример:
-     *
+     * <p>
      * 201
      * 196
      * 190
@@ -20,65 +25,100 @@ public class JavaAlgorithms {
      * 194
      * 193
      * 185
-     *
+     * <p>
      * Выбрать два момента времени, первый из них для покупки акций, а второй для продажи, с тем, чтобы разница
      * между ценой продажи и ценой покупки была максимально большой. Второй момент должен быть раньше первого.
      * Вернуть пару из двух моментов.
      * Каждый момент обозначается целым числом -- номер строки во входном файле, нумерация с единицы.
      * Например, для приведённого выше файла результат должен быть Pair(3, 4)
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
-    static public Pair<Integer, Integer> optimizeBuyAndSell(String inputName) {
-        throw new NotImplementedError();
+    static public Pair<Integer, Integer> optimizeBuyAndSell(String inputName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(inputName));
+        int maxLine = 0;
+        int exchangeDifference = 0;
+        int firstPrice = 0;
+        int secondIndex = 0;
+        List<Integer> arrPrice = new ArrayList<>();
+        List<Integer> arrPrice1 = new ArrayList<>();
+        Pair<Integer, Integer> indexMaxExchangeDifference;
+        indexMaxExchangeDifference = new Pair(1, 2);
+        int priceFirst = Integer.parseInt(br.readLine());
+        arrPrice.add(priceFirst);
+        arrPrice1.add(priceFirst);
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (maxLine > 0) {
+                arrPrice.add(Integer.parseInt(line));
+                arrPrice1.add(Integer.parseInt(line));
+                for (int j = 0; j < arrPrice1.size(); j++) {
+                    if (Integer.parseInt(line) < arrPrice1.get(j))
+                        arrPrice1.remove(j);
+                    else {
+                        if (Integer.parseInt(line) - arrPrice1.get(j) > exchangeDifference) {
+                            exchangeDifference = Integer.parseInt(line) - arrPrice1.get(j);
+                            firstPrice = arrPrice1.get(j);
+                            secondIndex = maxLine + 2;
+                        }
+                    }
+                }
+            }
+            maxLine++;
+        }
+        for (int j = 0; j < arrPrice.size(); j++) {
+            if (arrPrice.get(j) == firstPrice) return new Pair(j + 2, secondIndex);
+        }
+        br.close();
+        return indexMaxExchangeDifference;
     }
 
     /**
      * Задача Иосифа Флафия.
      * Простая
-     *
+     * <p>
      * Образовав круг, стоят menNumber человек, пронумерованных от 1 до menNumber.
-     *
+     * <p>
      * 1 2 3
      * 8   4
      * 7 6 5
-     *
+     * <p>
      * Мы считаем от 1 до choiceInterval (например, до 5), начиная с 1-го человека по кругу.
      * Человек, на котором остановился счёт, выбывает.
-     *
+     * <p>
      * 1 2 3
      * 8   4
      * 7 6 х
-     *
+     * <p>
      * Далее счёт продолжается со следующего человека, также от 1 до choiceInterval.
      * Выбывшие при счёте пропускаются, и человек, на котором остановился счёт, выбывает.
-     *
+     * <p>
      * 1 х 3
      * 8   4
      * 7 6 Х
-     *
+     * <p>
      * Процедура повторяется, пока не останется один человек. Требуется вернуть его номер (в данном случае 3).
-     *
+     * <p>
      * 1 Х 3
      * х   4
      * 7 6 Х
-     *
+     * <p>
      * 1 Х 3
      * Х   4
      * х 6 Х
-     *
+     * <p>
      * х Х 3
      * Х   4
      * Х 6 Х
-     *
+     * <p>
      * Х Х 3
      * Х   х
      * Х 6 Х
-     *
+     * <p>
      * Х Х 3
      * Х   Х
      * Х х Х
-     *
+     * <p>
      * Общий комментарий: решение из Википедии для этой задачи принимается,
      * но приветствуется попытка решить её самостоятельно.
      */
@@ -89,7 +129,7 @@ public class JavaAlgorithms {
     /**
      * Наибольшая общая подстрока.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например ОБСЕРВАТОРИЯ и КОНСЕРВАТОРЫ.
      * Найти их самую длинную общую подстроку -- в примере это СЕРВАТОР.
      * Если общих подстрок нет, вернуть пустую строку.
@@ -104,10 +144,10 @@ public class JavaAlgorithms {
     /**
      * Число простых чисел в интервале
      * Простая
-     *
+     * <p>
      * Рассчитать количество простых чисел в интервале от 1 до limit (включительно).
      * Если limit <= 1, вернуть результат 0.
-     *
+     * <p>
      * Справка: простым считается число, которое делится нацело только на 1 и на себя.
      * Единица простым числом не считается.
      */
