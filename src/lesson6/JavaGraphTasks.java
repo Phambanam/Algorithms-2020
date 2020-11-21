35517de5bbc22af9b7181cf3601aba806f08d946
Phambanam <54939419+Phambanam@users.noreply.github.com>
2020-11-21 14:25:58 +0300

    1

--- src/lesson6/JavaGraphTasks.java
+++ src/lesson6/JavaGraphTasks.java
@@ -1,12 +1,20 @@
+
 package lesson6;
 
 import kotlin.NotImplementedError;
 
-import java.util.List;
-import java.util.Set;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaGraphTasks {
+
+
     /**
      * Эйлеров цикл.
      * Средняя
@@ -34,7 +42,38 @@ public class JavaGraphTasks {
      * связного графа ровно по одному разу
      */
     public static List<Graph.Edge> findEulerLoop(Graph graph) {
-        throw new NotImplementedError();
+       List<Graph.Edge> result = new ArrayList<>();
+       if(graph.getEdges().size() == 0) return result;
+        Map<Graph.Vertex,Set<Graph.Vertex>> connect = new HashMap<>();
+        for(Graph.Vertex vertex : graph.getVertices()){
+            connect.put(vertex, graph.getNeighbors(vertex));
+            if(connect.get(vertex).size() % 2 != 0) return result;
+        }
+        Deque<Graph.Vertex> currentPath = new ArrayDeque<>();
+
+        Graph.Vertex firstVertex = graph.getVertices().iterator().next();
+        currentPath.add(firstVertex);
+        List<Graph.Vertex> way = new ArrayList<>();
+        while(currentPath.size() != 0){
+           Graph.Vertex current = currentPath.getLast();
+           if(connect.get(current).size() != 0){
+               Graph.Vertex  next = connect.get(current).iterator().next();
+               connect.get(current).remove(next);
+               connect.get(next).remove(current);
+               currentPath.addLast(next);
+
+           }else{
+               currentPath.removeLast();
+               way.add(current);
+           }
+        }
+        for(Graph.Vertex vertex : connect.keySet()){
+            if(connect.get(vertex).size() != 0) return result;
+        }
+        for(int i = 0; i < way.size() - 1; i++){
+            result.add(graph.getConnection(way.get(i),way.get(i+1)));
+        }
+        return result;
     }
 
     /**
@@ -119,8 +158,25 @@ public class JavaGraphTasks {
      *
      * Ответ: A, E, J, K, D, C, H, G, B, F, I
      */
+    public static Path bestPath;
+    private static void findBestPath(Graph graph, Graph.Vertex currVertex,Path currPath){
+        Graph.Vertex lastVertex = currPath.getVertices().get(currPath.getVertices().size() - 1);
+        for(Graph.Vertex vertex : graph.getNeighbors(lastVertex)){
+            if(currPath.contains(vertex)) continue;
+            Path newPath = new Path(currPath,graph,vertex);
+            if(bestPath.getLength() < newPath.getLength()) {
+                bestPath = newPath;
+            }
+            findBestPath(graph,vertex,newPath);
+        }
+    }
     public static Path longestSimplePath(Graph graph) {
-        throw new NotImplementedError();
+        bestPath = new Path();
+        for(Graph.Vertex vertex : graph.getVertices()){
+            if (bestPath.getVertices().size() == graph.getVertices().size()) break;
+             findBestPath(graph,vertex, new Path(vertex));
+        }
+        return bestPath;
     }
 
 
@@ -153,7 +209,66 @@ public class JavaGraphTasks {
      * В файле буквы разделены пробелами, строки -- переносами строк.
      * Остальные символы ни в файле, ни в словах не допускаются.
      */
-    static public Set<String> baldaSearcher(String inputName, Set<String> words) {
-        throw new NotImplementedError();
+    static private boolean test(boolean[][] booleans, String[][] array, String word, int row, int col) {
+        if (word.length() == 1) return true;
+        String str = String.valueOf(word.charAt(1));
+        String substring = word.substring(1);
+        if (row > 0 && array[row - 1][col].equals(str) && !booleans[row - 1][col]) {
+            booleans[row][col] = true;
+            if (test(booleans, array, substring, row - 1, col)) return true;
+        }
+        if (row + 1 < array.length && array[row + 1][col].equals(str) && !booleans[row + 1][col]) {
+            booleans[row][col] = true;
+            if (test(booleans, array, substring, row + 1, col)) return true;
+        }
+        if (col > 0 && array[row][col - 1].equals(str) && !booleans[row][col - 1]) {
+            booleans[row][col] = true;
+            if (test(booleans, array, substring, row, col - 1)) return true;
+        }
+        if (col + 1 < array[0].length && array[row][col + 1].equals(str) && !booleans[row][col + 1]) {
+            booleans[row][col] = true;
+            if (test(booleans, array, substring, row, col + 1)) return true;
+        }
+        booleans[row][col] = false;
+        return false;
     }
+
+    static public Set<String> baldaSearcher(String inputName, Set<String> words) throws IOException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File(inputName)));
+        String str = br.readLine();
+        List<String> list = new ArrayList<>();
+        while (str != null) {
+            list.add(str);
+            str = br.readLine();
+        }
+        String[][] arrayWords = new String[list.size()][list.get(0).length() / 2 + 1];
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length(); j += 2) {
+                arrayWords[i][j / 2] = String.valueOf(list.get(i).charAt(j));
+            }
+        }
+        Set<String> result = new HashSet<>();
+        boolean[][] booleans = new boolean[arrayWords.length][arrayWords[0].length];
+        int count;
+        for (String element : words) {
+            for (int i = 0; i < arrayWords.length; i++) {
+                count = 0;
+                for (int j = 0; j < arrayWords[0].length; j++) {
+                    if (arrayWords[i][j].equals(String.valueOf(element.charAt(0)))) {
+                        if (test(booleans, arrayWords, element, i, j)) {
+                            result.add(element);
+                            booleans = new boolean[arrayWords.length][arrayWords[0].length];
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                if (count == 1) break;
+            }
+        }
+        return result;
+    }
+
+    // трудоёмкост : O(n*m) - n это число строк и m это число букв в одной строке
+    // ресурсоёмкост : O(n*m)
 }
